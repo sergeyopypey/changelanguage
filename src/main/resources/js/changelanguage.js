64cb0de3909ba04d7a83b4bc7ae9654bb3f9bfe1
sergeyopypey <19.02.02@mail.ru>
2023-07-25 15:28:10 +0300

    Release

--- src/main/resources/js/changelanguage.js
+++ src/main/resources/js/changelanguage.js
@@ -1,9 +1,9 @@
 var htmlElement = `<li id="change-language">
-<a href="/plugins/servlet/applications/versions-licenses" id="admin_menu"
+<a href="/plugins/servlet/applications/versions-licenses" id="change-language_menu"
     class="aui-nav-link aui-dropdown2-trigger aui-dropdown2-trigger-arrowless" aria-label="Change-language"
     aria-haspopup="true" title="Change-language" resolved="" aria-controls="change-language-content"
     aria-expanded="false">
-    <span class="aui-icon aui-icon-small aui-iconfont-discover">Change-language</span>
+    <span class="aui-icon aui-icon-small aui-iconfont-world">Change-language</span>
 </a>
 <div id="change-language-content"
     class="aui-dropdown2 aui-style-default aui-layer aui-alignment-side-bottom aui-alignment-snap-auto" resolved=""
@@ -12,19 +12,44 @@ var htmlElement = `<li id="change-language">
     <div class="aui-dropdown2-section">
         <strong>Choose language</strong>
         <ul class="aui-list-truncate">
-            <li>
-                <a href="#" class="aui-nav-link" id="ru_RU-language" value="ru_RU" tabindex="-1">Русский</a>
-            </li>
-            <li>
-                <a href="#" class="aui-nav-link" id="en_US-language" value="en_US" tabindex="-1">English(US)</a>
-            </li>
         </ul>
     </div>
 </div>
 </li>`
 
+var options = {
+    "en_US": "English (United States)",
+    "zh_CN": "中文 (中国)",
+    "cs_CZ": "čeština (Česká republika)",
+    "da_DK": "Dansk (Danmark)",
+    "nl_NL": "Nederlands (Nederland)",
+    "en_UK": "English (UK)",
+    "et_EE": "Eesti (Eesti)",
+    "fi_FI": "suomi (Suomi)",
+    "fr_FR": "français (France)",
+    "de_DE": "Deutsch (Deutschland)",
+    "hu_HU": "magyar (Magyarország)",
+    "is_IS": "íslenska (Ísland)",
+    "it_IT": "italiano (Italia)",
+    "ja_JP": "日本語 (日本)",
+    "ko_KR": "한국어 (대한민국)",
+    "no_NO": "norsk (Norge)",
+    "pl_PL": "polski (Polska)",
+    "pt_BR": "português (Brasil)",
+    "ro_RO": "română (România)",
+    "ru_RU": "русский (Россия)",
+    "sk_SK": "Slovenčina (Slovenská republika)",
+    "es_ES": "español (España)",
+    "sv_SE": "svenska (Sverige)"
+};
+
 $(document).ready(function () {
     $(htmlElement).insertAfter($('#quicksearch-menu'))
+    $.each(options, function (value, text) {
+        var liElement = `<li><a href="#" class="aui-nav-link" id="${value}-language" value="${value}" tabindex="-1">${text}</a></li>`
+        $('#change-language-content ul.aui-list-truncate').append(liElement);
+
+    });
     $('#change-language-content a.aui-nav-link').click(function () {
         var lang = $(this).attr('value')
         var t = document.getElementById("atlassian-token");
